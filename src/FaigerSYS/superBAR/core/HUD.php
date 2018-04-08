d16af793fffe636bf70a5b89bdd286f8dc3b4328
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-04-08 14:33:52 +0100

    Update HUD.php

--- src/FaigerSYS/superBAR/core/HUD.php
+++ src/FaigerSYS/superBAR/core/HUD.php
@@ -156,8 +156,8 @@ class HUD
                 }
                 
                 if ($plugins['RankUp']) {
-                    $next_rank = $plugins['RankUp']->getCommand()->getRankUpCommand()->getNextRank($name);
-                    $rank_price = $plugins['RankUp']->getCommand()->getRankUpCommand()->getPrice($name);
+                    $next_rank = $plugins['RankUp']->getRankStore()->getNextRank($name);
+                    $rank_price = $plugins['RankUp']->getRank()->getPrice($name);
                 }
                 
                 if (($inv = $p->getInventory()) !== null) {
